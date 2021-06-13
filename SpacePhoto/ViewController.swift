6236684e5eabaed73675c06658c0047d57b81cc2
Gordon Choi <thefirate@gmail.com>
2021-06-14 00:17:26 +0900

    Initial Commit

---
+++ SpacePhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Gordon Choi on 2021/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
